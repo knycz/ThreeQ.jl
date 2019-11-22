28a1f0da75e8f2ddb1da9d1415b31b52cb58cc5b
Daniel O'Malley <omalled@lanl.gov>
2019-11-22 13:42:38 -0700

    update JuMP part

--- src/jump.jl
+++ src/jump.jl
@@ -1,7 +1,8 @@
-function bqp(Q, c, solver)
-	m = JuMP.Model(solver=solver)
+import LinearAlgebra
+function bqp(Q, c, optimizer)
+	m = JuMP.Model(optimizer)
 	@JuMP.variable(m, x[1:size(Q, 1)], Bin)
-	@JuMP.objective(m, Min, dot(x, Q * x) + dot(c, x))
-	JuMP.solve(m)
-	return m
+	@JuMP.objective(m, Min, LinearAlgebra.dot(x, Q * x) + LinearAlgebra.dot(c, x))
+	JuMP.optimize!(m)
+	return JuMP.value.(x), m
 end
